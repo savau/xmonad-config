f19cc054b32dfc14a334bdfc3f53e3099b9c24e2
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-07 16:09:53 +0000

    switch file manager to pcmanfm

--- xmonad.hs
+++ xmonad.hs
@@ -135,7 +135,7 @@ myStartupApplications =
 -- frequently used applications
 myFUAs :: Map KeySym String
 myFUAs = Map.fromList
-  [ (xK_f, "thunar"                         )  -- file manager
+  [ (xK_f, "pcmanfm"                        )  -- file manager
   , (xK_k, "keepassxc"                      )  -- password manager
   , (xK_w, "firefox"                        )  -- web browser
   , (xK_m, "LC_TIME=root.UTF-8 thunderbird" )  -- mail client
