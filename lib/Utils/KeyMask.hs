c3c881a365946e5133ff6dc88608c5ad8812d495
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-07-13 15:24:54 +0200

    Utils.KeyMask: add documentation

--- lib/Utils/KeyMask.hs
+++ lib/Utils/KeyMask.hs
@@ -5,8 +5,10 @@ module Utils.KeyMask
 import XMonad
 
 
+-- | Convenience definition for Mod1 (== Alt) key mask
 altMask :: KeyMask
 altMask = mod1Mask
 
+-- | Convenience definition for the "no" key mask (i.e. no key pressed)
 noMask :: KeyMask
 noMask = 0
