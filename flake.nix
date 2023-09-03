22b7fbe2995e10a9cf4175d6858c3f7d14203d9f
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-09-03 19:44:42 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,25 +3,35 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
-    home-manager.url = github:nix-community/home-manager;
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home, ... }: {
-    home.packages = with nixpkgs; [
-      dmenu
-    ];
+  outputs = { self, nixpkgs, home-manager, ... }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations."savau" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
 
-    xsession.windowManager.xmonad = {
-      enable = true;
-      enableContribAndExtras = true;
-      extraPackages = haskellPackages: with haskellPackages; [
-        xmonad xmonad-contrib xmonad-extras
-        dbus
-        gtk-sni-tray
-        status-notifier-item
-        tuple
+      home.packages = with nixpkgs; [
+        dmenu
       ];
-      config = builtins.readFile ./xmonad-monolith.hs; # TODO include modules from lib as build inputs and remove then-obsolete xmonad-monolith.hs
+
+      xsession.windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        extraPackages = haskellPackages: with haskellPackages; [
+          xmonad xmonad-contrib xmonad-extras
+          dbus
+          gtk-sni-tray
+          status-notifier-item
+          tuple
+        ];
+        config = builtins.readFile ./xmonad-monolith.hs; # TODO include modules from lib as build inputs and remove then-obsolete xmonad-monolith.hs
+      };
     };
   };
 }
