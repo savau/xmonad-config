c479f22efa0ab069ad945016730f8761cfac708c
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-09-03 18:33:54 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,27 +6,22 @@
     home-manager.url = github:nix-community/home-manager;
   };
 
-  outputs = { self, nixpkgs, home-manager, user, ... }: {
-    imports = [
-      home-manager.nixosModules.default
+  outputs = { self, nixpkgs, home, ... }: {
+    home.packages = with nixpkgs; [
+      dmenu
     ];
 
-    home-manager.users.${user} = {
-      home.packages = with nixpkgs; [
-        dmenu
+    xsession.windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages: with haskellPackages; [
+        xmonad xmonad-contrib xmonad-extras
+        dbus
+        gtk-sni-tray
+        status-notifier-item
+        tuple
       ];
-      xsession.windowManager.xmonad = {
-        enable = true;
-        enableContribAndExtras = true;
-        extraPackages = haskellPackages: with haskellPackages; [
-          xmonad xmonad-contrib xmonad-extras
-          dbus
-          gtk-sni-tray
-          status-notifier-item
-          tuple
-        ];
-        config = builtins.readFile ./xmonad-monolith.hs; # TODO include modules from lib as build inputs and remove then-obsolete xmonad-monolith.hs
-      };
+      config = builtins.readFile ./xmonad-monolith.hs; # TODO include modules from lib as build inputs and remove then-obsolete xmonad-monolith.hs
     };
   };
 }
