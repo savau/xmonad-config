1e3d291122b34dcaf1de455ef4d0270638d26894
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-11-10 00:09:05 +0100

    major cleanup

--- xmonad.hs
+++ xmonad.hs
@@ -46,7 +46,6 @@ import XMonad.Util.SpawnOnce (spawnOnce)
 
 
 main = do
---nScreens <- countScreens
   xmonad . docks . ewmh $ def
     { terminal           = "xfce4-terminal"
     , modMask            = myModMask
@@ -61,8 +60,8 @@ main = do
     , layoutHook  = avoidStruts myLayouts
     , manageHook  = manageHook def <+> manageDocks <+> manageSpawn <+> myManageFloats
     , startupHook = setWMName "LG3D"
-    -- , startupHook = mapM_ spawnApplication (Set.toList myStartupApplications) >> setWMName "LG3D"
     }
+
 
 myKeys :: XConfig Layout -> Map (ButtonMask, KeySym) (X ())
 myKeys conf = Map.fromList $ concat
@@ -89,6 +88,16 @@ myKeys conf = Map.fromList $ concat
       [ (wsKeySym  , windows $ (StackSet.greedyView . show) wsId) | Workspace{..} <- Set.toList myWorkspaces ]
               ++ ((, spawn "i3lock -n -c 000000") <$> Set.toList myLockScreenKeys)
               ++ ((, myXMonadSysPrompt) <$> Set.toList mySystemKeys)
+    myLockScreenKeys :: Set KeySym
+    myLockScreenKeys = Set.fromList
+      [ xK_minus   -- us layout
+      , xK_ssharp  -- german layout
+      ]
+    mySystemKeys :: Set KeySym
+    mySystemKeys = Set.fromList
+      [ xK_equal  -- us layout
+      , 65105     -- german layout; dead acute
+      ]
 
     mySystemCommands :: Map KeySym (X ())
     mySystemCommands =
@@ -138,65 +147,41 @@ myKeys conf = Map.fromList $ concat
         )
       ]
 
-myManageFloats :: ManageHook
-myManageFloats = composeAll
-  [ XMonad.appName =? "thunar"    --> doFloat
-  , XMonad.appName =? "keepassxc" --> doFloat
-  , XMonad.appName =? "nextcloud" --> doFloat
-  , XMonad.appName =? "element-desktop" --> doFloat
-  ]
+    myUWXPrompt :: XConfig Layout -> X ()
+    myUWXPrompt conf = xmonadPromptC (Map.toList myUWXPromptOpts) myUWXPromptConf where
+      myUWXPromptConf :: XPConfig
+      myUWXPromptConf = myXPromptConf
+        { defaultPrompter = const "UniWorX >>= "
+        , autoComplete    = Just 0
+        , font = "-*-liberation sans mono-medium-r-normal--0-0-0-0-m-0-*-*"
+        }
+      myUWXPromptOpts :: Map String (X ())
+      myUWXPromptOpts = Map.fromList
+        [ ( "u[2w]: develop@srv01.uniworx.de:~/u2w" , spawn $ XMonad.terminal conf <> " --execute " <> myUWXUtilsDir <> "/launch-terminal/dev.sh --develop u2w" )
+        , ( "f[radrive]: develop@srv01.uniworx.de:~/fradrive" , spawn $ XMonad.terminal conf <> " --execute " <> myUWXUtilsDir <> "/launch-terminal/dev.sh --develop fradrive" )
+    --  , ( "z[sh]: zsh@srv01.uniworx.de" , spawn $ XMonad.terminal conf <> " -e \"source " <> myUWXUtilsDir <> "launch-terminal/dev.sh --project u2w\"" )
+    --  , ( "n[ix-shell]: nix-shell@srv01.uniworx.de" , spawn $ XMonad.terminal conf <> " -e \"source " <> myUWXUtilsDir <> "launch-terminal/dev.sh --nix-shell u2w\"" )
+    --  , ( "m[onitor]: monitor servers", spawn $ XMonad.terminal conf <> " -e \"source " <> myUWXUtilsDir <> "monitor/all_servers.sh\"" )
+    --  , ( "l[ocal]: shell@localhost:~/u2w" , spawn $ XMonad.terminal conf <> " -e \"source " <> myUWXUtilsDir <> "launch-terminal/local.sh\"" )
+    --  , ( "s[shfs-]m[ount]: mount SSHFS" , spawn $ myUWXUtilsDir <> "sshfs/start.sh" )
+    --  , ( "s[shfs-]u[nmount]: unmount SSHFS" , spawn $ myUWXUtilsDir <> "sshfs/stop.sh" )
+        ]
+      myUWXUtilsDir = "~/.utils/uniworx"
 
-myUWXPrompt :: XConfig Layout -> X ()
-myUWXPrompt conf = xmonadPromptC (Map.toList myUWXPromptOpts) myUWXPromptConf where
-  myUWXPromptConf :: XPConfig
-  myUWXPromptConf = myXPromptConf
-    { defaultPrompter = const "UniWorX >>= "
-    , autoComplete    = Just 0
-    , font = "-*-liberation sans mono-medium-r-normal--0-0-0-0-m-0-*-*"
-    }
-  myUWXPromptOpts :: Map String (X ())
-  myUWXPromptOpts = Map.fromList
-    [ ( "u[2w]: develop@srv01.uniworx.de:~/u2w" , spawn $ XMonad.terminal conf <> " --execute " <> myUWXUtilsDir <> "/launch-terminal/dev.sh --develop u2w" )
-    , ( "f[radrive]: develop@srv01.uniworx.de:~/fradrive" , spawn $ XMonad.terminal conf <> " --execute " <> myUWXUtilsDir <> "/launch-terminal/dev.sh --develop fradrive" )
---  , ( "z[sh]: zsh@srv01.uniworx.de" , spawn $ XMonad.terminal conf <> " -e \"source " <> myUWXUtilsDir <> "launch-terminal/dev.sh --project u2w\"" )
---  , ( "n[ix-shell]: nix-shell@srv01.uniworx.de" , spawn $ XMonad.terminal conf <> " -e \"source " <> myUWXUtilsDir <> "launch-terminal/dev.sh --nix-shell u2w\"" )
---  , ( "m[onitor]: monitor servers", spawn $ XMonad.terminal conf <> " -e \"source " <> myUWXUtilsDir <> "monitor/all_servers.sh\"" )
---  , ( "l[ocal]: shell@localhost:~/u2w" , spawn $ XMonad.terminal conf <> " -e \"source " <> myUWXUtilsDir <> "launch-terminal/local.sh\"" )
---  , ( "s[shfs-]m[ount]: mount SSHFS" , spawn $ myUWXUtilsDir <> "sshfs/start.sh" )
---  , ( "s[shfs-]u[nmount]: unmount SSHFS" , spawn $ myUWXUtilsDir <> "sshfs/stop.sh" )
-    ]
-  myUWXUtilsDir = "~/.utils/uniworx"
-
--- Wrapper type to map workspaces 
-data Workspace = Workspace
-                 { wsId     :: Int
-                 , wsKeySym :: KeySym
-                 }
-                 deriving (Eq, Show, Read)
-
-instance Ord Workspace where
-  compare (Workspace i _) (Workspace j _) = compare i j
-
-myWorkspaces :: Set Workspace
-myWorkspaces = Set.fromList $ (\(wsId,wsKeySym) -> Workspace{..}) <$> (zip (10:[1..9]) [xK_0..xK_9])
-
-myPP :: PP
-myPP = xmobarPP
-  { ppCurrent = xmobarColor myFocusColor    mempty . wrap "[" "]"
-  , ppHidden  = xmobarColor myMainColorDark mempty
-  , ppUrgent  = xmobarColor myUrgentColor   mempty . wrap "!" "!"
-  , ppSep     = " | "
-  }
+    -- myXMonadSysPrompt :: X ()
+    -- myXMonadSysPrompt = myXMonadSysPromptXfce where
+    --   myXMonadSysPromptXfce = spawn "xfce4-session-logout"
+    --   myXMonadSysPromptXmonad = xmonadPromptC (Map.toList mySysPromptOpts) myXPromptConf{ defaultPrompter = const "System: ", autoComplete = Just 0 } where
+    --     mySysPromptOpts :: MonadIO m => Map String (m ())
+    --     mySysPromptOpts = Map.fromList
+    --       [ ( "Logout"       , io $ exitWith ExitSuccess)  -- FIXME: does not work with xfce4
+    --       , ( "Suspend"      , spawn "systemctl suspend")
+    --       , ( "Hibernate"    , spawn "systemctl hibernate")
+    --       , ( "Reboot"       , spawn "systemctl reboot")
+    --       , ( "Poweroff"     , spawn "systemctl poweroff")
+    --       ]
 
 
-myModMask :: KeyMask
-myModMask = mod4Mask  -- Mod == Super
-
-myMainColorLight, myMainColorDark, myFocusColor, myUrgentColor :: String
-myMainColorLight = "#fafafa"
-myMainColorDark  = "#242424"
-myFocusColor     = "#78aeed"
-myUrgentColor    = "#ed333b"
 
 myLayouts = 
               Mirror Grid
@@ -213,103 +198,53 @@ myLayouts =
             delta   = 3/100
             frac    = 1/2
 
-myXMonadRestart :: String
-myXMonadRestart = "xmonad --restart"
--- myXMonadRestart = (concatMap (\Application{..} -> "pkill " <> appName <> "; ") $ Set.toList myStartupApplications) <> "xmonad --restart"
-
-myLockScreenKeys :: Set KeySym
-myLockScreenKeys = Set.fromList
-  [ xK_minus   -- us layout
-  , xK_ssharp  -- german layout
+myManageFloats :: ManageHook
+myManageFloats = composeAll
+  [ XMonad.appName =? "thunar"    --> doFloat
+  , XMonad.appName =? "keepassxc" --> doFloat
+  , XMonad.appName =? "nextcloud" --> doFloat
+  , XMonad.appName =? "element-desktop" --> doFloat
   ]
 
-mySystemKeys :: Set KeySym
-mySystemKeys = Set.fromList
-  [ xK_equal  -- us layout
-  , 65105     -- german layout; dead acute
-  ]
 
-myCheckNetwork :: String
-myCheckNetwork = "pingcount=10; while [ $pingcount -gt 0 ]; do ping -n -c 1 1.1.1.1; rc=$?; if [[ $rc -eq 0 ]]; then ((pingcount = 0)); fi; ((pingcount = pingcount - 1)); sleep 0.5; done; "
+-- | Wrapper type to map workspaces
+data Workspace = Workspace
+                 { wsId     :: Int
+                 , wsKeySym :: KeySym
+                 }
+                 deriving (Eq, Show, Read)
 
-myXMonadSysPrompt :: X ()
-myXMonadSysPrompt = myXMonadSysPromptXfce where
-  myXMonadSysPromptXfce = spawn "xfce4-session-logout"
-  myXMonadSysPromptXmonad = xmonadPromptC (Map.toList mySysPromptOpts) myXPromptConf{ defaultPrompter = const "System: ", autoComplete = Just 0 } where
-    mySysPromptOpts :: MonadIO m => Map String (m ())
-    mySysPromptOpts = Map.fromList
-      [ ( "Logout"       , io $ exitWith ExitSuccess)  -- FIXME: does not work with xfce4
-      , ( "Suspend"      , spawn "systemctl suspend")
-      , ( "Hibernate"    , spawn "systemctl hibernate")
-      , ( "Reboot"       , spawn "systemctl reboot")
-      , ( "Poweroff"     , spawn "systemctl poweroff")
-      ]
+instance Ord Workspace where
+  compare (Workspace i _) (Workspace j _) = compare i j
 
-myXPromptConf :: XPConfig
-myXPromptConf    = def
-  { font         = "-*-liberation sans-medium-r-normal--0-0-0-0-p-0-0-*"
-  , height       = 25
-  , historySize  = 0
-  , position     = Top
-  , borderColor  = myMainColorDark
-  , bgColor      = myMainColorDark
-  , alwaysHighlight = True
-  , bgHLight = myMainColorDark
-  , fgHLight = myFocusColor
-  }
+myWorkspaces :: Set Workspace
+myWorkspaces = Set.fromList $ (\(wsId,wsKeySym) -> Workspace{..}) <$> (zip (10:[1..9]) [xK_0..xK_9])
 
 
+-- | Application type for managing spawnable applications and their meta data
 data Application = Application
-                   { appName        :: String
-                   , appEnvironment :: Map String String
-                   , appOptions     :: [String]
-                   , appWorkspace   :: Maybe WorkspaceId
-                   }
-                   deriving (Eq, Ord, Show, Read)
+  { appName        :: String
+  , appEnvironment :: Map String String
+  , appOptions     :: [String]
+  , appWorkspace   :: Maybe WorkspaceId
+  }
+  deriving (Eq, Ord, Show, Read)
 
 -- | Spawns a given application with the given environment and options, and if given on a specific workspace
 spawnApplication :: Application -> X ()
 spawnApplication Application{..} = maybe spawn spawnOn appWorkspace $ intercalate " " $ (fmap (\(k,v) -> k<>"="<>v<>" ") $ Map.toList appEnvironment) <> (appName : appOptions)
 
 
--- TODO: Make myStartupApplications redundant by moving autostart to nix-config
--- | Applications that are automatically launched after starting XMonad
--- myStartupApplications :: Set Application
--- myStartupApplications = (Set.fromList . fmap (uncurryN Application))
---   [
--- --  ( "xfce4-power-manager"
--- --  , mempty, mempty, mempty
--- --  )
--- --, ( "volumeicon"
--- --  , mempty, mempty, mempty
--- --  )
---     ( "nm-applet"
---     , mempty, mempty, mempty
---     )
---   , ( "blueman-applet"
---     , mempty, mempty, mempty
---     )
--- --, ( "pamac-tray"  -- TODO: launch this iff on Arch Linux
--- --  , mempty, mempty, mempty
--- --  )
--- --, ( "keepassxc"
--- --  , mempty, mempty, mempty
--- --  )
--- --, ( "megasync"
--- --  , Map.fromList [ ("QT_SCALE_FACTOR","1") ], mempty, mempty  -- setting QT_SCALE_FACTOR=1 as a workaround to avoid immediate segfault, see https://github.com/meganz/MEGAsync/issues/443
--- --  )
--- --, ( "birdtray"
--- --  , Map.fromList [ ("LC_TIME","root.UTF-8") ], mempty, mempty
--- --  )
--- --, ( "zulip"
--- --  , mempty, mempty, mempty
--- --  )
--- --, ( "signal-desktop-beta"
--- --  , mempty, mempty, Just "8"
--- --  )
---   ]
+myMainColorLight, myMainColorDark, myFocusColor, myUrgentColor :: String
+myMainColorLight = "#fafafa"
+myMainColorDark  = "#242424"
+myFocusColor     = "#78aeed"
+myUrgentColor    = "#ed333b"
 
 
+-- | Mod == Super
+myModMask :: KeyMask
+myModMask = mod4Mask
 
 -- | Convenience definition for Mod1 (== Alt) key mask
 altMask :: KeyMask
